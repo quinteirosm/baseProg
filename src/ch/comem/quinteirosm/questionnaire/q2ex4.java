894733df7a223feafe0c1d53bf4da89821291285
Miguel Quinteiros <quinteiros.m@gmail.com>
2021-10-15 09:10:09 +0200

    questionnaire 2

---
+++ src/ch/comem/quinteirosm/questionnaire/q2ex4.java
@@ -0,0 +1,10 @@
+package ch.comem.quinteirosm.questionnaire;
+
+public class q2ex4 {
+    public static void main(String[] args) {
+        float x=129.8f;
+        int y=(int)x;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
