008cb82cb0701c6a1cd2fcb8309fa3009e8d8360
Miguel Quinteiros <quinteiros.m@gmail.com>
2021-10-21 23:51:38 +0200

    ex 11 fini

--- src/ch/comem/quinteirosm/serie2/ex11_date.java
+++ src/ch/comem/quinteirosm/serie2/ex11_date.java
@@ -6,22 +6,35 @@ import java.util.Scanner;
 public class ex11_date {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in).useLocale(new Locale("fr-CH"));
-        int[] date = {0,0,0};
-        System.out.println("Saisissez le jour de la date en nombre");
-        date[0] = input.nextInt();
-        System.out.println("Saisissez le mois de la date en nombre");
-        date[1] = input.nextInt();
-        System.out.println("Saisissez l'année de la date en nombre");
-        date[2] = input.nextInt();
 
-        if((date[1]==4 || date[1]==6 || date[1]==9 || date[1]==11) && !(date[0]<=30)){
-            System.out.println("la date est erronée");
-            return;
-        }else if(date[1]==2 && !(date[0]<=28)){
-            System.out.println("la date est erronée");
-            return;
+        boolean valid = false;
+        String valide = "";
+
+        System.out.println("Saisissez le jour de la date en nombre");
+        int jour = input.nextInt();
+        System.out.println("Saisissez le mois de la date en nombre");
+        int mois = input.nextInt();
+        System.out.println("Saisissez l'année de la date en nombre");
+        int annee = input.nextInt();
+
+        if((mois==4 || mois==6 || mois==9 || mois==11) && jour<=30){
+            valid=true;
+        }else if((annee%4!=0 && annee%100==0) && jour<=28){
+            //fev non bisex
+            valid=true;
+        }else if(((annee%4==0 && annee%100!=0) || annee%400==0) && jour<=29){
+            //fev bisex
+            valid=true;
+        }else if(jour<=31&&mois<=12){
+            valid=true;
         }
 
-        // exercice pas fini :(
+        if(valid=true){
+            valide="valide";
+        }else{
+            valide="non-valide";
+        };
+
+        System.out.println("La date "+jour+"/"+mois+"/"+annee+" est "+valide);
     }
 }
