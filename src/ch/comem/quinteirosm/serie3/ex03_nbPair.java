7738919d6cf59c3b331c14aa63d15fbc8ee3c45b
Miguel Quinteiros <quinteiros.m@gmail.com>
2021-11-03 22:00:31 +0100

    s2ex345

--- src/ch/comem/quinteirosm/serie3/ex03_nbPair.java
+++ src/ch/comem/quinteirosm/serie3/ex03_nbPair.java
@@ -5,17 +5,17 @@ import java.util.Scanner;
 
 public class ex03_nbPair {
     public static void main(String[] args) {
-        int userInput = 0;
-        while (userInput<1) {
+        int userInput;
+        do {
             Scanner input = new Scanner(System.in).useLocale(new Locale("fr-CH"));
             System.out.println("Entrez le nombre de nombres pairs que vous voulez afficher");
             userInput = input.nextInt();
 
-            System.out.println("Liste des nombres pairs entre 1 et "+userInput+" :");
+            System.out.println("Liste des nombres pairs entre 1 et " + userInput + " :");
 
-            for (int i = 2; i <= userInput; i+=2) {
+            for (int i = 2; i <= userInput; i += 2) {
                 System.out.println(i);
             }
-        }
+        } while (userInput < 1);
     }
 }
---
+++ src/ch/comem/quinteirosm/serie3/ex05_nombreGrandPetit.java
@@ -0,0 +1,9 @@
+package ch.comem.quinteirosm.serie3;
+
+public class ex05_nombreGrandPetit {
+    public static void main(String[] args) {
+        String[] nombres={};
+
+        for(int i=0; i<nombres);
+    }
+}
