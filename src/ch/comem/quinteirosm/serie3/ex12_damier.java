7ae0fb3528ef499c11bb94b758913406038961af
quinteirosm <quinteiros.m@gmail.com>
2021-11-04 23:23:52 +0100

    ex12s3 fini

--- src/ch/comem/quinteirosm/serie3/ex12_damier.java
+++ src/ch/comem/quinteirosm/serie3/ex12_damier.java
@@ -4,9 +4,9 @@ public class ex12_damier {
     public static void main(String[] args) {
         for (int i=0; i<8; i++){
             for (int x=0; x<8; x++){
-                if(x%2==0){
+                if((i%2!=0 && x%2==0) || (i%2==0 && x%2!=0)){
                     System.out.print(" o");
-                }else if(x%2!=0){
+                }else if((i%2==0 && x%2==0) || (i%2!=0 && x%2!=0)){
                     System.out.print(" #");
                 }
             }
