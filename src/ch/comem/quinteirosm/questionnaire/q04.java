066818e732497916b8ef5ad251f0eafd5e8290de
Miguel Quinteiros <quinteiros.m@gmail.com>
2021-11-05 09:36:11 +0100

    corr s3ex01-12 + q04

---
+++ src/ch/comem/quinteirosm/questionnaire/q04.java
@@ -0,0 +1,7 @@
+package ch.comem.quinteirosm.questionnaire;
+
+public class q04 {
+    public static void main(String[] args) {
+        int a=5;int b=2;System.out.println(a/b);
+    }
+}
--- src/ch/comem/quinteirosm/serie3/ex12_damier.java
+++ src/ch/comem/quinteirosm/serie3/ex12_damier.java
@@ -2,12 +2,15 @@ package ch.comem.quinteirosm.serie3;
 
 public class ex12_damier {
     public static void main(String[] args) {
-        for (int i=0; i<8; i++){
-            for (int x=0; x<8; x++){
+        int largeur=8;
+        int longueur=8;
+
+        for (int i=0; i<longueur; i++){
+            for (int x=0; x<largeur; x++){
                 if((i%2!=0 && x%2==0) || (i%2==0 && x%2!=0)){
-                    System.out.print(" o");
-                }else if((i%2==0 && x%2==0) || (i%2!=0 && x%2!=0)){
-                    System.out.print(" #");
+                    System.out.print("  #");
+                }else{
+                    System.out.print("  o");
                 }
             }
             System.out.println("");
